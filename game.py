3220523711fc9f5111ed25f065ede85101bc7851
arzhrd <arshadmuhammed7594@gmail.com>
2025-11-09 16:33:31 +0530

    game code

--- game.py
+++ game.py
@@ -1,2 +1,24 @@
 import pygame
 import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+#font = pygame.font.SysFont('arial', 25)
+
+class Direction(Enum):
+    DOWN = 4
+
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 40
