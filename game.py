484f9a222f666d0c0d3000cfb58eb487258db2ef
arzhrd <arshadmuhammed7594@gmail.com>
2025-11-10 17:15:21 +0530

    game code

--- game.py
+++ game.py
@@ -61,3 +61,15 @@ class SnakeGameAI:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        # 2. move
+        self._move(action) # update the head
+        self.snake.insert(0, self.head)
+        
+        # 3. check if game over
+        reward = 0
+        game_over = False
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+
