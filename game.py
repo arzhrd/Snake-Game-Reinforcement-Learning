f5bc5e20e3972790043590e8b1936f1a437ce199
arzhrd <arshadmuhammed7594@gmail.com>
2025-11-10 17:11:54 +0530

    game code

--- game.py
+++ game.py
@@ -33,4 +33,18 @@ class SnakeGameAI:
         self.clock = pygame.time.Clock()
         self.reset()
 
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head,
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
 
