086e90eede09acf6a3ed3de37616037d993bbbb8
arzhrd <arshadmuhammed7594@gmail.com>
2025-11-11 22:16:51 +0530

    game code

--- game.py
+++ game.py
@@ -85,4 +85,15 @@ class SnakeGameAI:
         self.clock.tick(SPEED)
         # 6. return game over and score
         return reward, game_over, self.score
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.head
+        # hits boundary
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        # hits itself
+        if pt in self.snake[1:]:
+            return True
+
+        return False
 
